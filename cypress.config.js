5d368dc1f270d8ea6b12847cce03a80f36e2b03b
brunogsiq <brunogsiq@gmail.com>
2025-03-15 15:04:37 -0300

    Revisar projeto - 1 - revisar yaml quando constar 2 - revisar testes 3 - revisar informações 4 - revisar git ignore

--- cypress.config.js
+++ cypress.config.js
@@ -10,11 +10,12 @@ module.exports = defineConfig({
     timestamp: 'mmddyyyy_HHMMss',
   },
   e2e: {
+    baseUrl: 'https://alura-fotos.herokuapp.com',
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://alura-fotos.herokuapp.com',
+    
   },
 })
